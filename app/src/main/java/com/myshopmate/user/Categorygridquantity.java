23f69e0da635ca23239d6a6d5405dbfbf5655728
Mandar Shinde <shindemandar82@gmail.com>
2020-08-15 16:07:17 +0530

    Initial commit

--- app/src/main/java/com/myshopmate/user/Categorygridquantity.java
+++ app/src/main/java/com/myshopmate/user/Categorygridquantity.java
@@ -1,6 +1,7 @@
 package com.myshopmate.user;
 
 import android.view.View;
+import android.view.View;
 
 public interface Categorygridquantity {
   void  onClick(View view, int position, String ccId, String id);
